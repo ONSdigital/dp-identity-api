e81a195d7095f0c0639cfd2d164b4eac28b0ff5f
HeJones <helene.v.jones@gmail.com>
2021-04-28 17:33:33 +0100

    Update documentation to explain email validation regex

--- validation/validation.go
+++ validation/validation.go
@@ -4,10 +4,41 @@ import (
 	"regexp"
 )
 
-//Description of regex
-//Before @: the initial squared brackets matches one the characters the email can start with, there is no full stop in these characters. The following statement in the round brackets checks if two full stops are consecutive. The next statement after the or (|) but before the @ checks for the characters that match these symbols.
-//@ matches an @ symbol
-//After @: the domain name is matched on the characters in the square brackets and the \. checks if there are two consecutive full stops.
+// This regex checks entered email address and if match found, email address deemed valid, else invalid.
+//
+// Regex delimits entered email into its user and domain name sections using `@` character.
+//
+// Note:
+// —————
+// `(?:…)` - denotes a non-capturing group
+// `[…]`   - denotes a character set
+// `*`     - denotes zero or more matches
+// `+`     - denotes one or more matches
+// `?`     - denotes a non-greedy match
+//
+// User name section:
+// ——————————————————
+// Grouped(
+//   first character set ensures that email starts with either a valid character (one or more)
+//   Grouped(second character set ensures characters are present after an escaped `.` (one or more)) (zero or more)
+//   OR
+//   Grouped(third character set matching permitted ascii hex characters OR a fourth character set matching a different set of permitted ascii hex characters) (zero or more)
+//)
+//
+// Domain name section:
+// ————————————————————
+// Grouped(
+//   first character set ensures that email domain section starts with an alphanumeric character
+//   Grouped(second character set ensures alphanumeric/hyphen (`-`) characters followed by an alphanumeric character with a non-greedy match to first escaped `.` encountered) (one or more)
+//   Third character set ensures that character immediately after `.` is an alphanumeric character
+//   Grouped(fourth character set ensures alphanumeric/hyphen (`-`) characters) (zero or more) followed by fifth character set, ensuring domain name section ends in an alphanumeric character)
+//)
+//
+// Example:
+// ————————
+// my.name@myself.com - match found - valid
+// .my.name@myself.com - match not found - invalid
+// my.name@myself.com. - match not found - invalid
 var emailRegex = regexp.MustCompile(`^(?:[a-z0-9!#$%&'*+/=?^_` + "`" + `{|}~-]+(?:\.[a-z0-9-!#$%&'*+/=?^_` + "`" + `{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)$`)
 
 //IsEmailValid is a function to validate email addresses. Here an email is valid if it follows the standard rules for valid email addresses. The unit tests contain examples of what are considered valid or invalid email addresses. Here we do not validate on the domain name.
