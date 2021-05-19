55f94dd5512e73d7646de671e7ff8457d0ae4f38
Matt Nicks <matthew.nicks@methods.co.uk>
2021-05-19 11:00:23 +0100

    add ID token parser

--- models/tokens_models.go
+++ models/tokens_models.go
@@ -25,3 +25,14 @@ func (t *IdToken) SignToken(signingKey []byte) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, t.Claims)
 	return token.SignedString(signingKey)
 }
+
+func (t *IdToken) ParseWithoutValidating(tokenString string) error {
+	parser := new(jwt.Parser)
+	idToken, _, parsingErr := parser.ParseUnverified(tokenString, &IdClaims{})
+	if parsingErr != nil {
+		return parsingErr
+	}
+	idClaims := idToken.Claims.(*IdClaims)
+	t.Claims = *idClaims
+	return nil
+}
