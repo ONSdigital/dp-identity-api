a23126bae0c7737576a00135e35bab36f85491d2
Matt Nicks <matthew.nicks@methods.co.uk>
2021-05-21 18:52:16 +0100

    add basehandler constructs

--- api/api.go
+++ api/api.go
@@ -2,7 +2,10 @@ package api
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
+	"github.com/ONSdigital/dp-identity-api/models"
+	"net/http"
 
 	"github.com/ONSdigital/dp-identity-api/apierrorsdeprecated"
 	"github.com/ONSdigital/dp-identity-api/cognito"
@@ -18,6 +21,18 @@ type API struct {
 	ClientId       string
 	ClientSecret   string
 	ClientAuthFlow string
+}
+
+type baseHandler func(w http.ResponseWriter, r *http.Request, errorList *models.ErrorList)
+
+func (handler baseHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var errorList models.ErrorList
+	handler(w, r, &errorList)
+
+	if len(errorList.Errors) > 0 {
+		ctx := r.Context()
+		WriteErrorResponse(ctx, w, errorList)
+	}
 }
 
 //Setup function sets up the api and returns an api
@@ -40,8 +55,28 @@ func Setup(ctx context.Context, r *mux.Router, cognitoClient cognito.Client, use
 
 	r.HandleFunc("/tokens", api.TokensHandler(ctx)).Methods("POST")
 	// self used in paths rather than identifier as the identifier is JWT tokens passed in the request headers
-	r.HandleFunc("/tokens/self", api.SignOutHandler(ctx)).Methods("DELETE")
+	r.HandleFunc("/tokens/self", baseHandler(api.signOutHandler).ServeHTTP).Methods("DELETE")
 	r.HandleFunc("/tokens/self", api.RefreshHandler(ctx)).Methods("PUT")
 	r.HandleFunc("/users", api.CreateUserHandler(ctx)).Methods("POST")
 	return api, nil
+}
+
+func WriteErrorResponse(ctx context.Context, w http.ResponseWriter, errorList models.ErrorList) {
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(errorList.Status)
+
+	jsonResponse, err := json.Marshal(errorList)
+	if err != nil {
+		log.Event(ctx, "failed to marshal the error", log.Error(err), log.ERROR)
+		http.Error(w, "failed to marshal the error", http.StatusInternalServerError)
+		return
+	}
+
+	_, err = w.Write(jsonResponse)
+	if err != nil {
+		log.Event(ctx, "writing response failed", log.Error(err), log.ERROR)
+		http.Error(w, "failed to write http response", http.StatusInternalServerError)
+		return
+	}
 }
