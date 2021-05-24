251acd4d4eaa95c3d74a4cc80f728961dcafc901
Matt Nicks <matthew.nicks@methods.co.uk>
2021-05-24 11:51:10 +0100

    separate codes from descriptions declarations

--- models/error_content.go
+++ models/error_content.go
@@ -2,31 +2,37 @@ package models
 
 import "github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
 
+// API error codes
 const (
-	InvalidTokenError                      = "InvalidToken"
-	MissingAuthorizationTokenDescription   = "no Authorization token was provided"
-	MalformedAuthorizationTokenDescription = "the authorization token does not meet the required format"
-	JSONMarshalError                       = "JSONMarshalError"
-	ErrorMarshalFailedDescription          = "failed to marshal the error"
-	WriteResponseError                     = "WriteResponseError"
-	WriteResponseFailedDescription         = "failed to write http response"
-	InternalError                          = "InternalServerError"
-	NotFoundError                          = "NotFound"
-	AlreadyExistsError                     = "AlreadyExists"
-	DeliveryFailureError                   = "DeliveryFailure"
-	InvalidCodeError                       = "InvalidCode"
-	ExpiredCodeError                       = "ExpiredCode"
-	InvalidFieldError                      = "InvalidField"
-	InvalidPasswordError                   = "InvalidPassword"
-	LimitExceededError                     = "LimitExceeded"
-	NotAuthorisedError                     = "NotAuthorised"
-	PasswordResetRequiredError             = "PasswordResetRequired"
-	TooManyFailedAttemptsError             = "TooManyFailedAttempts"
-	TooManyRequestsError                   = "TooManyRequests"
-	UserNotConfirmedError                  = "UserNotConfirmed"
-	UsernameExistsError                    = "UsernameExists"
+	InvalidTokenError          = "InvalidToken"
+	JSONMarshalError           = "JSONMarshalError"
+	WriteResponseError         = "WriteResponseError"
+	InternalError              = "InternalServerError"
+	NotFoundError              = "NotFound"
+	AlreadyExistsError         = "AlreadyExists"
+	DeliveryFailureError       = "DeliveryFailure"
+	InvalidCodeError           = "InvalidCode"
+	ExpiredCodeError           = "ExpiredCode"
+	InvalidFieldError          = "InvalidField"
+	InvalidPasswordError       = "InvalidPassword"
+	LimitExceededError         = "LimitExceeded"
+	NotAuthorisedError         = "NotAuthorised"
+	PasswordResetRequiredError = "PasswordResetRequired"
+	TooManyFailedAttemptsError = "TooManyFailedAttempts"
+	TooManyRequestsError       = "TooManyRequests"
+	UserNotConfirmedError      = "UserNotConfirmed"
+	UsernameExistsError        = "UsernameExists"
 )
 
+// API error descriptions
+const (
+	MissingAuthorizationTokenDescription   = "no Authorization token was provided"
+	MalformedAuthorizationTokenDescription = "the authorization token does not meet the required format"
+	ErrorMarshalFailedDescription          = "failed to marshal the error"
+	WriteResponseFailedDescription         = "failed to write http response"
+)
+
+// Mapping Cognito error codes to API error codes
 var cognitoErrorMapping = map[string]string{
 	cognitoidentityprovider.ErrCodeInternalErrorException:          InternalError,
 	cognitoidentityprovider.ErrCodeCodeDeliveryFailureException:    DeliveryFailureError,
