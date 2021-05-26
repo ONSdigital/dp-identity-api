970fb7bfa980776eaac0b0c9046bb69235cf05a2
Matt Nicks <matthew.nicks@methods.co.uk>
2021-05-26 15:12:01 +0100

    add success response writer function and expect a success response back from the handler

--- api/api.go
+++ api/api.go
@@ -13,6 +13,12 @@ import (
 	"github.com/gorilla/mux"
 )
 
+var (
+	IdTokenHeaderName      = "ID"
+	AccessTokenHeaderName  = "Authorization"
+	RefreshTokenHeaderName = "Refresh"
+)
+
 //API provides a struct to wrap the api around
 type API struct {
 	Router         *mux.Router
@@ -23,14 +29,16 @@ type API struct {
 	ClientAuthFlow string
 }
 
-type baseHandler func(w http.ResponseWriter, r *http.Request, ctx context.Context) *models.ErrorResponse
+type baseHandler func(w http.ResponseWriter, r *http.Request, ctx context.Context) (*models.SuccessResponse, *models.ErrorResponse)
 
 func contextAndErrors(h baseHandler) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		ctx := req.Context()
-		if err := h(w, req, ctx); err != nil {
+		response, err := h(w, req, ctx)
+		if err != nil {
 			writeErrorResponse(ctx, w, err)
 		}
+		writeSuccessResponse(ctx, w, response)
 	}
 }
 
@@ -52,7 +60,7 @@ func Setup(ctx context.Context, r *mux.Router, cognitoClient cognito.Client, use
 		ClientAuthFlow: clientAuthFlow,
 	}
 
-	r.HandleFunc("/tokens", api.TokensHandler(ctx)).Methods("POST")
+	r.HandleFunc("/tokens", contextAndErrors(api.TokensHandler)).Methods("POST")
 	// self used in paths rather than identifier as the identifier is JWT tokens passed in the request headers
 	r.HandleFunc("/tokens/self", contextAndErrors(api.SignOutHandler)).Methods("DELETE")
 	r.HandleFunc("/tokens/self", api.RefreshHandler(ctx)).Methods("PUT")
@@ -61,7 +69,6 @@ func Setup(ctx context.Context, r *mux.Router, cognitoClient cognito.Client, use
 }
 
 func writeErrorResponse(ctx context.Context, w http.ResponseWriter, errorResponse *models.ErrorResponse) {
-
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(errorResponse.Status)
 
@@ -80,16 +87,24 @@ func writeErrorResponse(ctx context.Context, w http.ResponseWriter, errorRespons
 	}
 }
 
-func handleBodyReadError(ctx context.Context, err error) *models.ErrorResponse {
-	return &models.ErrorResponse{
-		Errors: []error{models.NewError(ctx, err, models.BodyReadError, models.BodyReadFailedDescription)},
-		Status: http.StatusInternalServerError,
+func writeSuccessResponse(ctx context.Context, w http.ResponseWriter, successResponse *models.SuccessResponse) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(successResponse.Status)
+
+	_, err := w.Write(successResponse.Body)
+	if err != nil {
+		responseErr := models.NewError(ctx, err, models.WriteResponseError, models.WriteResponseFailedDescription)
+		http.Error(w, responseErr.Description, http.StatusInternalServerError)
+		return
 	}
 }
 
+func handleBodyReadError(ctx context.Context, err error) *models.ErrorResponse {
+	return models.NewErrorResponse([]error{models.NewError(ctx, err, models.BodyReadError, models.BodyReadFailedDescription)},
+		http.StatusInternalServerError)
+}
+
 func handleBodyUnmarshalError(ctx context.Context, err error) *models.ErrorResponse {
-	return &models.ErrorResponse{
-		Errors: []error{models.NewError(ctx, err, models.JSONUnmarshalError, models.ErrorUnmarshalFailedDescription)},
-		Status: http.StatusInternalServerError,
-	}
+	return models.NewErrorResponse([]error{models.NewError(ctx, err, models.JSONUnmarshalError, models.ErrorUnmarshalFailedDescription)},
+		http.StatusInternalServerError)
 }
