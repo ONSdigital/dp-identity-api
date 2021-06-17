3cef36c0d448395312a2d59c9bb7e902a1083f24
Matt Nicks <matthew.nicks@methods.co.uk>
2021-06-17 17:02:24 +0100

    add unit tests for the change password handler

--- api/users_test.go
+++ api/users_test.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -16,6 +18,7 @@ import (
 )
 
 const usersEndPoint = "http://localhost:25600/v1/users"
+const changePasswordEndPoint = "http://localhost:25600/v1/users/self/password"
 
 func TestCreateUserHandler(t *testing.T) {
 
@@ -225,6 +228,131 @@ func TestCreateUserHandler(t *testing.T) {
 				castErr = errorResponse.Errors[1].(*models.Error)
 				So(castErr.Code, ShouldEqual, tt.errorCodes[1])
 			}
+		}
+	})
+}
+
+func TestChangePasswordHandler(t *testing.T) {
+
+	var (
+		routeMux                                        = mux.NewRouter()
+		ctx                                             = context.Background()
+		poolId, clientId, clientSecret, authFlow string = "us-west-11_bxushuds", "abc123", "bsjahsaj9djsiq", "authflow"
+		email, password, session                 string = "foo_bar123@ext.ons.gov.uk", "Password2", "auth-challenge-session"
+		accessToken, idToken, refreshToken       string = "aaaa.bbbb.cccc", "llll.mmmm.nnnn", "zzzz.yyyy.xxxx.wwww.vvvv"
+		expireLength                             int64  = 500
+	)
+
+	m := &mock.MockCognitoIdentityProviderClient{}
+
+	api, _ := Setup(ctx, routeMux, m, poolId, clientId, clientSecret, authFlow)
+	w := httptest.NewRecorder()
+
+	Convey("RespondToAuthChallenge - check expected responses", t, func() {
+		respondToAuthChallengeTests := []struct {
+			respondToAuthChallengeFunction func(input *cognitoidentityprovider.RespondToAuthChallengeInput) (*cognitoidentityprovider.RespondToAuthChallengeOutput, error)
+			httpResponse                   int
+		}{
+			{
+				// Cognito successful password change
+				func(input *cognitoidentityprovider.RespondToAuthChallengeInput) (*cognitoidentityprovider.RespondToAuthChallengeOutput, error) {
+					return &cognitoidentityprovider.RespondToAuthChallengeOutput{
+						AuthenticationResult: &cognitoidentityprovider.AuthenticationResultType{
+							AccessToken:  &accessToken,
+							ExpiresIn:    &expireLength,
+							IdToken:      &idToken,
+							RefreshToken: &refreshToken,
+						},
+					}, nil
+				},
+				http.StatusAccepted,
+			},
+			{
+				// Cognito internal error
+				func(input *cognitoidentityprovider.RespondToAuthChallengeInput) (*cognitoidentityprovider.RespondToAuthChallengeOutput, error) {
+					awsErrCode := "InternalErrorException"
+					awsErrMessage := "Something strange happened"
+					awsOrigErr := errors.New(awsErrCode)
+					awsErr := awserr.New(awsErrCode, awsErrMessage, awsOrigErr)
+					return nil, awsErr
+				},
+				http.StatusInternalServerError,
+			},
+			{
+				// Cognito bad request
+				func(input *cognitoidentityprovider.RespondToAuthChallengeInput) (*cognitoidentityprovider.RespondToAuthChallengeOutput, error) {
+					awsErrCode := "NotAuthorizedException"
+					awsErrMessage := "session invlaid"
+					awsOrigErr := errors.New(awsErrCode)
+					awsErr := awserr.New(awsErrCode, awsErrMessage, awsOrigErr)
+					return nil, awsErr
+				},
+				http.StatusBadRequest,
+			},
+		}
+
+		for _, tt := range respondToAuthChallengeTests {
+			m.RespondToAuthChallengeFunc = tt.respondToAuthChallengeFunction
+
+			postBody := map[string]interface{}{"type": models.NewPasswordRequiredType, "email": email, "password": password, "session": session}
+			body, _ := json.Marshal(postBody)
+			r := httptest.NewRequest(http.MethodPut, changePasswordEndPoint, bytes.NewReader(body))
+
+			successResponse, errorResponse := api.ChangePasswordHandler(ctx, w, r)
+
+			// Check whether testing a success or error case
+			if tt.httpResponse > 399 {
+				So(successResponse, ShouldBeNil)
+				So(errorResponse.Status, ShouldEqual, tt.httpResponse)
+			} else {
+				So(successResponse.Status, ShouldEqual, tt.httpResponse)
+				So(errorResponse, ShouldBeNil)
+			}
+		}
+	})
+
+	Convey("RespondToAuthChallenge returns 500: error unmarshalling request body", t, func() {
+		r := httptest.NewRequest(http.MethodPut, changePasswordEndPoint, bytes.NewReader(nil))
+
+		successResponse, errorResponse := api.CreateUserHandler(ctx, w, r)
+
+		So(successResponse, ShouldBeNil)
+		castErr := errorResponse.Errors[0].(*models.Error)
+		So(castErr.Code, ShouldEqual, models.JSONUnmarshalError)
+		So(castErr.Description, ShouldEqual, models.ErrorUnmarshalFailedDescription)
+	})
+
+	Convey("Validation fails 400: validation of a required param throws validation errors", t, func() {
+		validationTests := []struct {
+			requestBody  map[string]interface{}
+			errorCode    string
+			httpResponse int
+		}{
+			// missing password change type
+			{
+				map[string]interface{}{"type": "", "email": email, "password": password, "session": session},
+				models.UnknownRequestTypeError,
+				http.StatusBadRequest,
+			},
+			// missing a change request param
+			{
+				map[string]interface{}{"type": models.NewPasswordRequiredType, "email": "", "password": password, "session": session},
+				models.InvalidEmailError,
+				http.StatusBadRequest,
+			},
+		}
+
+		for _, tt := range validationTests {
+			body, _ := json.Marshal(tt.requestBody)
+			r := httptest.NewRequest(http.MethodPut, changePasswordEndPoint, bytes.NewReader(body))
+
+			successResponse, errorResponse := api.ChangePasswordHandler(ctx, w, r)
+
+			So(successResponse, ShouldBeNil)
+			So(errorResponse.Status, ShouldEqual, tt.httpResponse)
+			So(len(errorResponse.Errors), ShouldEqual, 1)
+			castErr := errorResponse.Errors[0].(*models.Error)
+			So(castErr.Code, ShouldEqual, tt.errorCode)
 		}
 	})
 }
