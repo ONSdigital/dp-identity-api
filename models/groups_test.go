53d0ff10baa0fc6335f8610d4d96d788eab86cf6
Matt Nicks <matthew.nicks@methods.co.uk>
2021-07-13 10:26:35 +0100

    add unit tests to the group model for adding a user to a group

--- models/groups_test.go
+++ models/groups_test.go
@@ -1,6 +1,7 @@
 package models_test
 
 import (
+	"context"
 	"github.com/ONSdigital/dp-identity-api/models"
 	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
 	"reflect"
@@ -30,6 +31,69 @@ func TestNewPublisherRoleGroup(t *testing.T) {
 		So(adminGroup.Name, ShouldEqual, models.PublisherRoleGroup)
 		So(adminGroup.Description, ShouldEqual, description)
 		So(adminGroup.Precedence, ShouldEqual, precedence)
+	})
+}
+
+func TestGroup_ValidateAddUser(t *testing.T) {
+	var ctx = context.Background()
+
+	Convey("returns InvalidUserId error if no user id is submitted", t, func() {
+		group := models.Group{
+			Name: "test-group",
+		}
+		userPoolId := "abcd-efgh-1234"
+		userId := ""
+
+		errs := group.ValidateAddUser(ctx, userPoolId, userId)
+
+		So(errs, ShouldNotBeNil)
+		So(len(errs), ShouldEqual, 1)
+		castErr := errs[0].(*models.Error)
+		So(castErr.Code, ShouldEqual, models.InvalidUserIdError)
+		So(castErr.Description, ShouldEqual, models.MissingUserIdErrorDescription)
+	})
+
+	Convey("returns InvalidGroupName error if no group name is set", t, func() {
+		group := models.Group{}
+		userPoolId := "abcd-efgh-1234"
+		userId := "zzzz-9999"
+
+		errs := group.ValidateAddUser(ctx, userPoolId, userId)
+
+		So(errs, ShouldNotBeNil)
+		So(len(errs), ShouldEqual, 1)
+		castErr := errs[0].(*models.Error)
+		So(castErr.Code, ShouldEqual, models.InvalidGroupNameError)
+		So(castErr.Description, ShouldEqual, models.MissingGroupNameErrorDescription)
+	})
+
+	Convey("returns InvalidUserId and InvalidGroupName errors if no user id submitted and group name set", t, func() {
+		group := models.Group{}
+		userPoolId := "abcd-efgh-1234"
+		userId := ""
+
+		errs := group.ValidateAddUser(ctx, userPoolId, userId)
+
+		So(errs, ShouldNotBeNil)
+		So(len(errs), ShouldEqual, 2)
+		castErr := errs[0].(*models.Error)
+		So(castErr.Code, ShouldEqual, models.InvalidGroupNameError)
+		So(castErr.Description, ShouldEqual, models.MissingGroupNameErrorDescription)
+		castErr = errs[1].(*models.Error)
+		So(castErr.Code, ShouldEqual, models.InvalidUserIdError)
+		So(castErr.Description, ShouldEqual, models.MissingUserIdErrorDescription)
+	})
+
+	Convey("returns nil if user id is present", t, func() {
+		group := models.Group{
+			Name: "test-group",
+		}
+		userPoolId := "abcd-efgh-1234"
+		userId := "zzzz-9999"
+
+		errs := group.Validate(ctx, userPoolId, userId)
+
+		So(errs, ShouldBeNil)
 	})
 }
 
@@ -68,5 +132,22 @@ func TestGroups_BuildGetGroupRequest(t *testing.T) {
 		So(reflect.TypeOf(*response), ShouldEqual, reflect.TypeOf(cognitoidentityprovider.GetGroupInput{}))
 		So(*response.UserPoolId, ShouldEqual, userPoolId)
 		So(*response.GroupName, ShouldEqual, group.Name)
+	})
+}
+
+func TestGroups_BuildAddUserToGroupRequest(t *testing.T) {
+	Convey("builds a correctly populated Cognito GetGroup request body", t, func() {
+		group := models.Group{
+			Name: "role-test",
+		}
+		userPoolId := "euwest-99-aabbcc"
+		userId := "zzzz-9999"
+
+		response := group.BuildAddUserToGroupRequest(userPoolId)
+
+		So(reflect.TypeOf(*response), ShouldEqual, reflect.TypeOf(cognitoidentityprovider.AdminAddUserToGroupInput{}))
+		So(*response.UserPoolId, ShouldEqual, userPoolId)
+		So(*response.GroupName, ShouldEqual, group.Name)
+		So(*response.Username, ShouldEqual, userId)
 	})
 }
