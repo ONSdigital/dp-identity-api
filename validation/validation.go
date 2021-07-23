2bb7bb4c665672787e61f51e35c5678f756dd805
Matt Nicks <matthew.nicks@methods.co.uk>
2021-07-23 15:11:43 +0100

    remove unused regex

--- validation/validation.go
+++ validation/validation.go
@@ -41,7 +41,6 @@ import (
 // .my.name@myself.com - match not found - invalid
 // my.name@myself.com. - match not found - invalid
 var emailRegex = regexp.MustCompile(`^(?:[a-z0-9!#$%&'*+/=?^_` + "`" + `{|}~-]+(?:\.[a-z0-9-!#$%&'*+/=?^_` + "`" + `{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)$`)
-var onsEmailRegex = regexp.MustCompile(`^(?:[a-z0-9!#$%&'*+/=?^_` + "`" + `{|}~-]+(?:\.[a-z0-9-!#$%&'*+/=?^_` + "`" + `{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(:?ext\.)?ons.gov.uk$`)
 var (
 	minimumEmailLength, maximumEmailLength int = 3, 254
 )
