6f7761fd9ab396ae7ada40953e49e4bfc2d0f6f1
Peter Idah <peter.idah@gmail.com>
2022-08-22 17:13:39 +0100

    make get-jwks-keys retrieves JWKS public keys for setting the JWT_VERIFICATION_PUBLIC_KEYS environment variable

---
+++ scripts/get-jwks-keys/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/ONSdigital/dp-identity-api/jwks"
+)
+
+func main() {
+	jwksHandler := &jwks.JWKS{}
+
+	USER_POOL_ID, ok := os.LookupEnv("USER_POOL_ID")
+	if !ok {
+		fmt.Println("ensure the USER_POOL_ID environment variavble is set.")
+		os.Exit(2)
+	}
+
+	REGION, ok := os.LookupEnv("REGION")
+	if !ok {
+		fmt.Println("ensure the REGION  environment variavble is set.")
+		os.Exit(2)
+	}
+
+	jwksRSAKeys, err := jwksHandler.GetJWKSRSAKeys(REGION, USER_POOL_ID)
+	if err != nil {
+		log.Fatal("could not retrieve the JWKS RSA public keys", err)
+	}
+
+	keys := []string{}
+	for k, v := range jwksRSAKeys {
+		key := fmt.Sprintf("%s:%s", k, v)
+		keys = append(keys, key)
+	}
+	pubKeyString := strings.Join(keys, ",")
+	colored := fmt.Sprintf("\x1b[%dm%s\x1b[0m", 32, pubKeyString)
+	fmt.Println(colored)
+}
