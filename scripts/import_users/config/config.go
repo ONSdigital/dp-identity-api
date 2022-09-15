75ed4b3e60ad68e6874f16fb1bbd872f55fb6bcc
Nathan Shumoogum <nathan.shumoogum@methods.co.uk>
2022-09-15 17:03:44 +0100

    Update config requirements for scripts

--- scripts/import_users/config/config.go
+++ scripts/import_users/config/config.go
@@ -11,7 +11,7 @@ type Config struct {
 	GroupUsersFilename   string `envconfig:"GROUPUSERS_FILENAME" required:"true"`
 	UserFileName         string `envconfig:"FILENAME" required:"true"`
 	S3Bucket             string `envconfig:"S3_BUCKET" required:"true"`
-	S3BaseDir            string `envconfig:"S3_BASE_DIR" required:"true"`
+	S3BaseDir            string `envconfig:"S3_BASE_DIR"`
 	S3Region             string `envconfig:"S3_REGION" required:"true"`
 	AWSCognitoUserPoolID string `envconfig:"USER_POOL_ID" required:"true"`
 	AWSProfile           string `envconfig:"AWS_PROFILE" required:"true"`
