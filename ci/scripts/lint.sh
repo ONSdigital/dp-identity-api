512bf2310c58268b44a9a2ee9d27f5944b74979c
cookel2 <eleanor.cook@ons.gov.uk>
2025-03-19 16:11:51 +0000

    upgrade linter to latest version

--- ci/scripts/lint.sh
+++ ci/scripts/lint.sh
@@ -5,6 +5,6 @@ cwd=$(pwd)
 npm install -g @redocly/cli
 
 pushd $cwd/dp-identity-api
-  go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.61.0
+  go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.64.8
   make lint
 popd
