e7bf354f942043acae95409bb16eb501b29dbd4f
cookel2 <eleanor.cook@ons.gov.uk>
2025-04-08 14:44:59 +0100

    correct the error message to fix last component test

--- cognito/mock/cognito_stub.go
+++ cognito/mock/cognito_stub.go
@@ -128,7 +128,7 @@ func (m *CognitoIdentityProviderClientStub) InitiateAuth(_ context.Context, inpu
 			} else if user.Email != input.AuthParameters["USERNAME"] {
 				return nil, &smithy.GenericAPIError{
 					Code:    errCodeNotAuthorized,
-					Message: "Incorrect username or password",
+					Message: "Incorrect username or password.",
 				}
 			} else if user.Password != input.AuthParameters["PASSWORD"] {
 				return nil, &smithy.GenericAPIError{
